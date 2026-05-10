9f76deb9ba0698163d3b62311a588eca5b2d22c8
Randy Eckman <emanspeaks@gmail.com>
2026-05-10 01:21:19 -0500

    add winreg stuff, add mkdircase

---
+++ pkgs/msys2/pre-userprofile.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# this file is sourced by pull.sh, so any functions or variables there are available here
+
+# Set Win10 context menu as default
+# local ctx_menu_script="$DOTPKGDIR/msys2/scripts/restore_win10_context_menu_default.bat"
+local reg_changes=0
+win_reg_ensure_value 'HKEY_CURRENT_USER\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32'
+((reg_changes += $?))
+win_reg_ensure_value 'HKEY_CURRENT_USER\SOFTWARE\CLASSES\CLSID\{d93ed569-3b3e-4bff-8355-3c44f6a52bb5}\InprocServer32'
+((reg_changes += $?))
+
+# Disable Bing search in Start menu
+win_reg_ensure_value 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search' 'BingSearchEnabled' REG_DWORD 0x0
+((reg_changes += $?))
+win_reg_ensure_value 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search' 'AllowSearchToUseLocation' REG_DWORD 0x0
+((reg_changes += $?))
+win_reg_ensure_value 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search' 'CortanaConsent' REG_DWORD 0x0
+((reg_changes += $?))
+
+if [[ $reg_changes -gt 0 ]]; then
+  info "Some registry changes were made that may require restarting explorer.exe to take effect."
+  read -p "Do you want to restart explorer.exe now? [Y/n] " -n 1 yn
+  if [[ $yn =~ ^[Yy]$ || -z "$yn" ]]; then
+    warn "Restarting explorer.exe"
+    taskkill //f //im explorer.exe
+    start explorer.exe
+  fi
+fi
--- utils/msys2helpers.sh
+++ utils/msys2helpers.sh
@@ -14,3 +14,50 @@ get_win_env_assocref() {
     assocref["$key"]="$value"
   done
 }
+
+win_reg_get_value() {
+  # returns [key, value, type, data] in the resultref array
+  local -n resultref="$1"
+  local key="$2"
+  local value="$3"
+  local valflag
+  [ -z "$value" ] && valflag='//ve' || valflag='//v'
+  local regout
+  resultref=()  # clear resultref in case reg query fails
+  # do not quote vars here; will cause syntax error in reg query
+  regout=$(reg query $key $valflag $value) || return 1
+  resultref=($(tr -d '\r' <<< "$regout"))
+}
+
+win_reg_set_value() {
+  local key="$1"
+  local value="$2"
+  local type="$3"
+  local data="$4"
+  local valflag
+  local typeflag
+  local dataflag
+  [ -z "$value" ] && valflag='//ve' || valflag='//v'
+  [ -z "$type" ] || typeflag="/t $type"
+  [ -z "$data" ] || dataflag="/d $data"
+  # do not quote vars here; will cause syntax error in reg add
+  reg add $key /f $valflag $value $typeflag $type $dataflag $data || return 1
+}
+
+win_reg_ensure_value() {
+  local key="$1"
+  local value="$2"
+  local type="$3"
+  local data="$4"
+  local result=()
+  local needupdate=0
+  win_reg_get_value result "$key" "$value" || needupdate=1
+  local matchtype="${type:-REG_SZ}"
+  if [[ $needupdate -eq 1 || "${result[2]}" != "$matchtype" || "${result[3]}" != "$data" ]]; then
+    debug "$BASH_SOURCE" "$LINENO" "Upserting reg $key :: $value type=$type data=$data"
+    win_reg_set_value "$key" "$value" "$type" "$data" || die "Failed to set reg $key :: $value type=$type data=$data"
+    return 1
+  else
+    debug "$BASH_SOURCE" "$LINENO" "No reg change needed: $key :: $value type=$type data=$data"
+  fi
+}
