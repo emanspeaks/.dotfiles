583be07471623cc76e99b23c7e436758ad6d042c
Randy Eckman <emanspeaks@gmail.com>
2026-07-22 16:15:24 -0500

    webdev: update fnm installer, add to throwbeadsnswim

--- pkgs/webdev/pre-userprofile.sh
+++ pkgs/webdev/pre-userprofile.sh
@@ -3,4 +3,8 @@
 
 if [[ $(get_machine_name) != worklaptop ]]; then
   winget install Schniz.fnm
+elif ! command -v fnm &> /dev/null; then
+  curl -LO https://github.com/Schniz/fnm/releases/download/v1.39.0/fnm-windows.zip -o $TEMP/fnm-windows.zip
+  unzip $TEMP/fnm-windows.zip -d ~/.local/bin
+  rm $TEMP/fnm-windows.zip
 fi
